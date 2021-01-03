cc6f1e5e6c05b712bebae7ade4a59f4da129ca5b
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-01-03 21:04:47 +0530

    deployment settings

---
+++ build/precache-manifest.83b6c3ab0759f303deb30a312fd2e212.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c0eddb38878b63c60ac29f53b274ad8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6d4ec18c9621ec80506",
+    "url": "/static/css/main.de2f560c.chunk.css"
+  },
+  {
+    "revision": "da1dff4ed080829fd93f",
+    "url": "/static/js/2.dff00327.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dff00327.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6d4ec18c9621ec80506",
+    "url": "/static/js/main.87a4eacc.chunk.js"
+  },
+  {
+    "revision": "559ea9b268b7819359f3",
+    "url": "/static/js/runtime-main.786b8bbc.js"
+  }
+]);
